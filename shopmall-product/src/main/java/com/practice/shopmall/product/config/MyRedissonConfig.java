f0ec559e2f51297b270051e3f49cfcf88fbcc4b0
Terry Lee <p134030772@gmail.com>
2021-10-11 13:47:26 +0800

    Revert "redis config"
    This reverts commit 389b79ca

--- shopmall-product/src/main/java/com/practice/shopmall/product/config/MyRedissonConfig.java
+++ shopmall-product/src/main/java/com/practice/shopmall/product/config/MyRedissonConfig.java
@@ -3,7 +3,6 @@ package com.practice.shopmall.product.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +12,9 @@ import java.io.IOException;
 public class MyRedissonConfig {
 
     @Bean(destroyMethod="shutdown")
-    RedissonClient redisson(@Value("${spring.redis.host}") String url,@Value("${spring.redis.port}") String port) throws IOException {
+    RedissonClient redisson() throws IOException {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://" + url + ":" + port);
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         RedissonClient redissonClient = Redisson.create(config);
         return redissonClient;
     }
