e64b8eec370dbb7ed216c33a325cd863c2297986
Terry Lee <p134030772@gmail.com>
2021-10-24 15:43:11 +0800

    add resources

---
+++ shopmall-gateway/src/test/java/com/practice/shopmall/gateway/ShopmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.shopmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
